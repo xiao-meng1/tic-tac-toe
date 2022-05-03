0126ff5bc77d794bf3b7c41c0c87501fe1b7668d
Xiao <xmeng1@ualberta.ca>
2022-05-03 15:14:45 -0600

    add html layout and css styling

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const gameBoard = (() => {
         Array(3).fill(undefined)
     ];
     const printGrid = () => console.log(grid);
-    const addMove = (rowIndex, columnIndex, symbol) => {
-        grid[rowIndex][columnIndex] = symbol;
+    const addMove = (rowIndex, colIndex, symbol) => {
+        grid[rowIndex][colIndex] = symbol;
         printGrid();
     }
 
@@ -14,12 +14,24 @@ const gameBoard = (() => {
 })();
 
 const createPlayer = (name, symbol) => {
-    const playMove = (rowIndex, columnIndex) => {
-        gameBoard.addMove(rowIndex, columnIndex, symbol);
+    const playMove = () => {
+        const rowIndex = Number(prompt("Select a row to play in from 0 to 2"));
+        const colIndex = Number(prompt("Select a column to play in from 0 to 2"));
+        return {rowIndex, colIndex};
     }
 
     return {name, symbol, playMove}
-}
+};
 
 const playerOne = createPlayer("Player One", "X");
 const playerTwo = createPlayer("Player Two", "O");
+
+const game = (() => {
+    const playTurn = (player) => {
+        const {rowIndex, colIndex} = player.playMove();
+        console.log(rowIndex, colIndex);
+        gameBoard.addMove(rowIndex, colIndex, player.symbol);
+    };
+
+    return {playTurn};
+})();
