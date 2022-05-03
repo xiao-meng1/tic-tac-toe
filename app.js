3fe062689bfa8200f01dd8bb6005c07b427547b2
Xiao <xmeng1@ualberta.ca>
2022-05-03 11:54:00 -0600

    add createPlayer factory function

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const gameBoard = (() => {
     const printGrid = () => console.log(grid);
     const addMove = (rowIndex, columnIndex, symbol) => {
         grid[rowIndex][columnIndex] = symbol;
+        printGrid();
     }
 
     return {printGrid, addMove};
 })();
+
+const createPlayer = (name, symbol) => {
+    const playMove = (rowIndex, columnIndex) => {
+        gameBoard.addMove(rowIndex, columnIndex, symbol);
+    }
+
+    return {name, symbol, playMove}
+}
+
+const playerOne = createPlayer("Player One", "X");
+const playerTwo = createPlayer("Player Two", "O");
